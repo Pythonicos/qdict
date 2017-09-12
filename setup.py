e4d49a40b07a0fbbf353a84381a4cf804b4c2d6b
ohrlando <ohrlando@gmail.com>
2017-09-12 20:59:00 -0300

    update version

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(name='qdict',
-      version='1.0',
+      version='1.1',
       description='A tool for query iterable of objects',
       author='Ohrlando',
       author_email='pythonicos@outlook.com',
       url='https://github.com/Pythonicos/qdict',
-      py_modules=['qdict/__init__', 'qdict/exceptions'],
+      py_modules=['qdict/__init__', 'qdict/exceptions', 'qdict/__version__'],
       license='MIT',
       classifiers=['Programming Language :: Python :: 3'],
       keywords='query list dictionary search tool',
