a4895c339542ff4788fb819268a5c27ccc43be06
ohrlando <ohrlando@gmail.com>
2017-09-11 21:46:53 -0300

    added setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='qdict',
+      version='1.0',
+      description='A tool for query iterable of objects',
+      author='Ohrlando',
+      author_email='pythonicos@outlook.com',
+      url='https://github.com/Pythonicos/qdict',
+      py_modules=['qdict/__init__', 'qdict/exceptions'])
